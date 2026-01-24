f0207528e566e9a93031acd01d2b84fca2e8d749
Timppav <156774864+Timppav@users.noreply.github.com>
2026-01-24 06:59:31 +0200

    Enemy & spawn warning animations

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,10 +4,16 @@ using UnityEngine.AI;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] AudioClip _deathSound;
-    EntityHealth _entityHealth;
+    [SerializeField] SpriteRenderer _characterBody;
+    [SerializeField] Animator _animator;
+    [SerializeField] float _idleDuration = 2f;
 
+    EntityHealth _entityHealth;
     NavMeshAgent _agent;
     GameObject _target;
+    Vector3 _lastPosition;
+    float _idleTimer;
+    bool _isIdle = true;
 
     void Awake()
     {
@@ -15,18 +21,37 @@ public class Enemy : MonoBehaviour
 
         _agent = GetComponent<NavMeshAgent>();
         _agent.updateRotation = false;
+        _agent.updateRotation = false;
+        _agent.updateUpAxis = false;
     }
 
     void Start()
     {
+        _idleTimer = _idleDuration;
         _target = GameObject.FindGameObjectWithTag("Player");
+        _lastPosition = transform.position;
 
         _entityHealth.OnDeath += DestroyEnemy;
     }
 
     void Update()
     {
-        _agent.SetDestination(_target.transform.position);
+        if (_isIdle)
+        {
+            _idleTimer -= Time.deltaTime;
+            if (_idleTimer <= 0f)
+            {
+                _isIdle = false;
+            }
+            return;
+        }
+        
+        if (_target != null && _agent.enabled)
+        {
+            _agent.SetDestination(_target.transform.position);
+            HandleAnimation();
+            HandleSpriteFlip();
+        }
     }
 
     void OnDisable()
@@ -34,9 +59,30 @@ public class Enemy : MonoBehaviour
         _entityHealth.OnDeath -= DestroyEnemy;
     }
 
+    void HandleAnimation()
+    {
+        bool isMoving = _agent.velocity.magnitude > 0.1f;
+        _animator.SetBool("isWalking", isMoving);
+    }
+
+    void HandleSpriteFlip()
+    {
+        // Flip sprite based on movement direction
+        if (_agent.velocity.x < -0.1f)
+        {
+            _characterBody.flipX = true;
+        }
+        else if (_agent.velocity.x > 0.1f)
+        {
+            _characterBody.flipX = false;
+        }
+    }
+
     public void DestroyEnemy()
     {
+        _animator.SetBool("isDead", true);
+        _agent.enabled = false;
         AudioManager.Instance.PlayAudio(_deathSound, AudioManager.SoundType.SFX, 1.0f, false);
-        Destroy(gameObject);
+        Destroy(gameObject, 0.6f);
     }
 }
